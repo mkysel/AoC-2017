4df04b3bd38677817f9126ee07fe6674d2081ac1
Martin Kysel <mkysel@nuodb.com>
2017-12-18 18:50:28 +0100

    16/1

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -1,7 +1,31 @@
+SMALL_INPUT = "s1,x3/4,pe/b"
+
 with open("input.txt", "r") as f:
     INPUT = f.read()
 
 def solve_challenge(val):
-    return 0
+    #dancers = list("abcde")
+    dancers = list("abcdefghijklmnop")
 
-print solve_challenge(INPUT)
+    for move in val:
+        if move[0] == "s":
+            steps = int(move[1:])
+            dancers = (dancers[-steps:] + dancers[:-steps])
+
+        elif move[0] == "p":
+            left, right = move[1:].split("/")
+            idx_l = dancers.index(left)
+            idx_r = dancers.index(right)
+
+            dancers[idx_l], dancers[idx_r] = dancers[idx_r], dancers[idx_l]
+
+        else: # x
+            left, right = move[1:].split("/")
+            idx_l = (int(left)) % len(dancers)
+            idx_r = (int(right)) % len(dancers)
+
+            dancers[idx_l], dancers[idx_r] = dancers[idx_r], dancers[idx_l]
+
+    return ''.join(dancers)
+
+print solve_challenge(INPUT.split(","))
