77f5887219540633a7bc97397d609bbb429a587c
Martin Kysel <mkysel@nuodb.com>
2017-12-18 19:26:39 +0100

    17/1

--- Day17/Day17.py
+++ Day17/Day17.py
@@ -1,7 +1,18 @@
+SMALL_INPUT = "3"
+
 with open("input.txt", "r") as f:
     INPUT = f.read()
 
-def solve_challenge(val):
-    return 0
 
-print solve_challenge(INPUT)
+def solve_challenge(val):
+    buffer = [0]
+    current_position = 0
+
+    for next_val in xrange(1, 2018):
+        next_position = (current_position + val) % len(buffer)
+        buffer = buffer[:next_position+1] + [next_val] + buffer[next_position+1:]
+        current_position = next_position+1
+
+    return buffer[current_position+1]
+
+print solve_challenge(int(INPUT))
