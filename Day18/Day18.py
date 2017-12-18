04e931a903ed14a447cc43cc74cf1298883e4002
Martin Kysel <mkysel@nuodb.com>
2017-12-18 09:36:29 +0100

    stage 1

--- Day18/Day18.py
+++ Day18/Day18.py
@@ -1,7 +1,55 @@
+from collections import defaultdict
+
+SMALL_INPUT = '''set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2'''
+
 with open("input.txt", "r") as f:
     INPUT = f.read()
 
-def solve_challenge(val):
-    return 0
 
-print solve_challenge(INPUT)
+def solve_challenge(val):
+    idx = 0
+    registry = defaultdict(int)
+    send = None
+
+    while True:
+        if val[idx][0] == "set":
+            try:
+                registry[val[idx][1]] = int(val[idx][2])
+            except ValueError:
+                registry[val[idx][1]] = registry[val[idx][2]]
+        elif val[idx][0] == "add":
+            try:
+                registry[val[idx][1]] += int(val[idx][2])
+            except ValueError:
+                registry[val[idx][1]] += registry[val[idx][2]]
+        elif val[idx][0] == "mul":
+            try:
+                registry[val[idx][1]] *= int(val[idx][2])
+            except ValueError:
+                registry[val[idx][1]] *= registry[val[idx][2]]
+        elif val[idx][0] == "mod":
+            try:
+                registry[val[idx][1]] %= int(val[idx][2])
+            except ValueError:
+                registry[val[idx][1]] %= registry[val[idx][2]]
+        elif val[idx][0] == "snd":
+            send = registry[val[idx][1]]
+        elif val[idx][0] == "rcv":
+            if registry[val[idx][1]] != 0:
+                return send
+        elif val[idx][0] == "jgz":
+            if registry[val[idx][1]] > 0:
+                    idx += int(val[idx][2])
+                    continue
+        idx += 1
+
+print solve_challenge(map(lambda x: x.split(" "), INPUT.split("\n")))
