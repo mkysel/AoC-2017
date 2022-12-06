24b454c08fc93fe692bc51ed574dea3b89ffad26
Martin Kysel <code@martinkysel.com>
2022-12-06 13:14:58 -0300

    catching up on 2017 yay

--- Day19/Day19.py
+++ Day19/Day19.py
@@ -1,7 +1,29 @@
-with open("input.txt", "r") as f:
-    INPUT = f.read()
+import sys, re
 
-def solve_challenge(val):
-    return 0
+def solve_challenge(input):
+    data = [[c if c != ' ' else 0 for c in x.strip('\n')] for x in input]
 
-print solve_challenge(INPUT)
+    dx, dy = 0, 1
+    y = 0
+    x = data[y].index('|')
+    seen = []
+    steps = 0
+    while data[y][x]:
+        seen.extend(re.findall('[A-Z]', data[y][x]))
+        steps += 1
+        if data[y][x] == '+':
+            if dx:
+                dx, dy = 0, 1 if data[y + 1][x] else -1
+            else:
+                dx, dy = 1 if data[y][x + 1] else -1, 0
+        x += dx
+        y += dy
+
+    print(''.join(seen))
+    print(steps)
+
+
+if __name__ == '__main__':
+    with open("input.txt", "r") as f:
+        INPUT = f.readlines()
+    solve_challenge(INPUT)
